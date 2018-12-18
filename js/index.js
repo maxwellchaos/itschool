3baf326761f7a39bacec12eb9e1044b6378709df
anton-sergeenkov <warnes23@yandex.ru>
2018-12-18 23:24:41 +0300

    vuejs, adaptive menu

--- js/index.js
+++ js/index.js
@@ -1,113 +1,105 @@
 
-getCourses();
-function getCourses() {
-
-	var result = '';
-	var arr = [
-		{img:'_tmpimg/courses.jpg', text:'Программирование на C# для ребят от 11 до 17 лет', btn1:'', btn2:''},
-		{img:'_tmpimg/courses.jpg', text:'Web - разработка. Возрастные ограничения от 16 лет', btn1:'', btn2:''},
-		{img:'_tmpimg/courses.jpg', text:'Фотошоп', btn1:'', btn2:''},
-		{img:'_tmpimg/courses.jpg', text:'3d моделирование для школьников от 14 лет для взрослых', btn1:'', btn2:''},
-		{img:'_tmpimg/courses.jpg', text:'Еще какой-то курс', btn1:'', btn2:''}
-	];
-
-    for(var i=0; i<=arr.length-1; i++) {
-        result += `
-        	<div class="item">
-				<div class="img" style="background-image: url(${arr[i].img});"></div>
-				<div class="text">${arr[i].text}</div>
-				<div class="buttons">
-					<button class="btn-standart">Подробнее</button>
-					<button class="btn-standart">Записаться на пробное бесплатное занятие</button>
-				</div>
-			</div>
-        `;
-    }
-
-	$('.courses-container').html(result);
-}
-
-
-getTeachers();
-function getTeachers() {
-
-	var result = '';
-	var arr = [
-		{img:'_tmpimg/teachers.jpg', name:'Холуев Максим Александрович', text:'разработчик с 15-летним стажем, CRM-консультант, преподавал программирование в университете, также занимается маркетингом, активно выступает на различных конференциях как докладчик.'},
-		{img:'_tmpimg/teachers.jpg', name:'Холуев Максим Александрович', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non? Iusto, nulla! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non?'},
-		{img:'_tmpimg/teachers.jpg', name:'Холуев Максим Александрович', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non? Iusto, nulla! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non?'},
-		{img:'_tmpimg/teachers.jpg', name:'Холуев Максим Александрович', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non? Iusto, nulla! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non?'}
-	];
-
-    for(var i=0; i<=arr.length-1; i++) {
-        result += `
-        	<div class="item">
-        		<div class="name">${arr[i].name}</div>
-				<div class="img" style="background-image: url(${arr[i].img});"></div>
-				<div class="text">${arr[i].text}</div>
-			</div>
-        `;
-    }
-
-	$('.teachers-container').html(result);
-}
+const store = new Vuex.Store({
+	state: {
+		storeMenu: [
+			{link:'courses.html', name:'Программирование на C#'},
+			{link:'courses.html', name:'Web-разработка'},
+			{link:'courses.html', name:'Фотошоп'},
+			{link:'courses.html', name:'3D моделирование'}
+		],
+		storeCourses: [
+			{color:'orange', img:'_tmpimg/courses.jpg', text:'Программирование на C# для ребят от 11 до 17 лет',       btn1:'', btn2:''},
+			{color:'orange', img:'_tmpimg/courses.jpg', text:'Web - разработка. Возрастные ограничения от 16 лет',     btn1:'', btn2:''},
+			{color:'orange', img:'_tmpimg/courses.jpg', text:'Фотошоп',                                                btn1:'', btn2:''},
+			{color:'orange', img:'_tmpimg/courses.jpg', text:'3d моделирование для школьников от 14 лет для взрослых', btn1:'', btn2:''},
+			{color:'orange', img:'_tmpimg/courses.jpg', text:'Еще какой-то курс',                                      btn1:'', btn2:''}
+		],
+		storeTeachers: [
+			{img:'_tmpimg/teachers.jpg', name:'Холуев Максим Александрович', text:'разработчик с 15-летним стажем, CRM-консультант, преподавал программирование в университете, также занимается маркетингом, активно выступает на различных конференциях как докладчик.'},
+			{img:'_tmpimg/teachers.jpg', name:'Холуев Максим Александрович', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non? Iusto, nulla! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non?'},
+			{img:'_tmpimg/teachers.jpg', name:'Холуев Максим Александрович', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non? Iusto, nulla! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non?'},
+			{img:'_tmpimg/teachers.jpg', name:'Холуев Максим Александрович', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non? Iusto, nulla! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sed quis non?'}
+		],
+		storeSteps: [
+			{img:'_tmpimg/step.png', text:'1,5-2 часа'},
+			{img:'_tmpimg/step.png', text:'2 занятия в неделю'},
+			{img:'_tmpimg/step.png', text:'Только практика'},
+			{img:'_tmpimg/step.png', text:'- По будням вечером <br> - Весь день в выходные'},
+			{img:'_tmpimg/step.png', text:'Видео и методичка по урокам для домашних работ'},
+			{img:'_tmpimg/step.png', text:'Дарим подарки, выдаем сертификаты'}
+		],
+		storeReviews: [
+			{img:'_tmpimg/teachers.jpg', name:'Zoe Saldana', text:'Я записала своего сына мы попробовали и теперь он программист. Поступил в ПГУ. Хочет продолжать карьеру программиста.', link:'https://vk.com/'},
+			{img:'_tmpimg/teachers.jpg', name:'Zoe Saldana', text:'Я записала своего сына мы попробовали и теперь он программист. Поступил в ПГУ. Хочет продолжать карьеру программиста.', link:'https://vk.com/'},
+			{img:'_tmpimg/teachers.jpg', name:'Zoe Saldana', text:'Я записала своего сына мы попробовали и теперь он программист. Поступил в ПГУ. Хочет продолжать карьеру программиста.', link:'https://vk.com/'},
+			{img:'_tmpimg/teachers.jpg', name:'Zoe Saldana', text:'Я записала своего сына мы попробовали и теперь он программист. Поступил в ПГУ. Хочет продолжать карьеру программиста.', link:'https://vk.com/'}
+		]
+	},
+	mutations: {
+		// setEventsActive(state, payload) {
+		// 	state.eventsTime[payload.num].active = payload.active
+		// }
+	}
+})
 
 
-
-getSteps();
-function getSteps() {
-
-	var result = '';
-	var arr = [
-		{img:'_tmpimg/step.png', text:'1,5-2 часа'},
-		{img:'_tmpimg/step.png', text:'2 занятия в неделю'},
-		{img:'_tmpimg/step.png', text:'Только практика'},
-		{img:'_tmpimg/step.png', text:'- По будням вечером <br> - Весь день в выходные'},
-		{img:'_tmpimg/step.png', text:'Видео и методичка по урокам для домашних работ'},
-		{img:'_tmpimg/step.png', text:'Дарим подарки, выдаем сертификаты'}
-	];
-
-    for(var i=0; i<=arr.length-1; i++) {
-        result += `
-        	<div class="item">
-        		<div class="img" style="background-image: url(${arr[i].img});"></div>
-        		<div class="text">${arr[i].text}</div>
-			</div>
-        `;
-    }
-
-	$('.steps-container').html(result);
-}
-
-
-
-
-getReviews();
-function getReviews() {
-
-	var result = '';
-	var arr = [
-		{img:'_tmpimg/teachers.jpg', name:'Zoe Saldana', text:'Я записала своего сына мы попробовали и теперь он программист. Поступил в ПГУ. Хочет продолжать карьеру программиста.', link:'https://vk.com/'},
-		{img:'_tmpimg/teachers.jpg', name:'Zoe Saldana', text:'Я записала своего сына мы попробовали и теперь он программист. Поступил в ПГУ. Хочет продолжать карьеру программиста.', link:'https://vk.com/'},
-		{img:'_tmpimg/teachers.jpg', name:'Zoe Saldana', text:'Я записала своего сына мы попробовали и теперь он программист. Поступил в ПГУ. Хочет продолжать карьеру программиста.', link:'https://vk.com/'},
-		{img:'_tmpimg/teachers.jpg', name:'Zoe Saldana', text:'Я записала своего сына мы попробовали и теперь он программист. Поступил в ПГУ. Хочет продолжать карьеру программиста.', link:'https://vk.com/'}
-	];
-
-    for(var i=0; i<=arr.length-1; i++) {
-        result += `
-        	<div class="item">
-        		<div class="img" style="background-image: url(${arr[i].img});"></div>
-        		<div class="wrapper">
-					<div class="name">${arr[i].name}</div>
-					<div class="text">${arr[i].text}</div>
-					<a href="${arr[i].link}" target="_blank">Ссылка ВК</a>
-        		</div>
-			</div>
-        `;
-    }
-
-	$('.reviews-container').html(result);
-}
+var appMenu = new Vue({
+	el: '#vue-menu',
+	store,
+	computed: {
+		storeMenu() { return store.state.storeMenu; }
+	}
+});
+var appCourses = new Vue({
+	el: '#vue-courses',
+	store,
+	computed: {
+		storeCourses() { return store.state.storeCourses; }
+	}
+});
+var appTeachers = new Vue({
+	el: '#vue-teachers',
+	store,
+	computed: {
+		storeTeachers() { return store.state.storeTeachers; }
+	}
+});
+var appSteps = new Vue({
+	el: '#vue-steps',
+	store,
+	computed: {
+		storeSteps() { return store.state.storeSteps; }
+	}
+});
+var appReviews = new Vue({
+	el: '#vue-reviews',
+	store,
+	computed: {
+		storeReviews() { return store.state.storeReviews; }
+	}
+});
 
 
+$('.reviews-container').slick({
+	infinite: true,
+	slidesToShow: 2,
+	slidesToScroll: 2,
+	responsive: [
+		{breakpoint: 1200,  settings:  { slidesToShow: 1, slidesToScroll: 1 }}
+	]
+});
+
+
+var headerMenu = '.header-menu';
+
+$(headerMenu+'-touch').on('click', function() {
+	$(headerMenu).slideToggle();
+	$(this).toggleClass('open');
+});
+
+$(window).on('resize', function() {
+	if($(headerMenu).is(':hidden')) {
+		$(headerMenu).removeAttr('style');
+	}
+});
 
--- module/slick-slider/custom.js
+++
@@ -1,9 +0,0 @@
-
-$('.reviews-container').slick({
-	infinite: true,
-	slidesToShow: 2,
-	slidesToScroll: 2,
-	responsive: [
-		{breakpoint: 1200,  settings:  { slidesToShow: 1, slidesToScroll: 1 }}
-	]
-});
